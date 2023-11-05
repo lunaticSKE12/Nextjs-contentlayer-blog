ee97f969aa975bdf45af2fa63cb58244d58a761b
lunaticSKE12 <jamesnapongd@gmail.com>
2023-11-05 17:00:15 +0700

    Navbar component

---
+++ src/components/Header/index.js
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import Logo from './Logo';
+import {
+  DribbbleIcon,
+  GithubIcon,
+  LinkedinIcon,
+  SunIcon,
+  TwitterIcon,
+} from '../Icons';
+
+const Header = () => {
+  return (
+    <header className="w-full p-4 flex items-center justify-between">
+      <Logo />
+      <nav
+        className="w-max py-3 px-8 border border-solid border-dark rounded-full 
+      font-medium capitalize flex items-center fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm"
+      >
+        <Link href={'/'} className="mr-2">
+          Home
+        </Link>
+        <Link href={'/about'} className="mx-2">
+          About
+        </Link>
+        <Link href={'/contact'} className="mr-2">
+          Contact
+        </Link>
+        <button>
+          <SunIcon />
+        </button>
+      </nav>
+      <div>
+        <a href="http://example.com" className="w-6 h-6 mr-4 inline-block">
+          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href="http://example.com" className="w-6 h-6 mr-4 inline-block">
+          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href="http://example.com" className="w-6 h-6 mr-4 inline-block">
+          <GithubIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href="http://example.com" className="w-6 h-6 mr-4 inline-block">
+          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
