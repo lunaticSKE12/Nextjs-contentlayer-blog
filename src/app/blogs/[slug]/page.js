69388a0659216b006e0e4cfbb726849dc2bd3258
lunaticSKE12 <jamesnapongd@gmail.com>
2023-11-10 19:51:18 +0700

    Creating TOC Component

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -35,7 +35,42 @@ export default function BlogPage({ params }) {
       </div>
       <BlogDetails blog={blog} slug={params.slug} />
       <div className="grid grid-cols-12  gap-y-8 lg:gap-8 sxl:gap-16 mt-8 px-5 md:px-10">
-        <div className="col-span-4">toc</div>
+        <div className="col-span-4">
+          <details
+            className="border-[1px] border-solid border-dark text-dark  rounded-lg p-4 sticky top-6 max-h-[80vh] overflow-hidden overflow-y-auto"
+            open
+          >
+            <summary className="text-lg font-semibold capitalize cursor-pointer">
+              Table Of Content
+            </summary>
+            <ul className="mt-4 font-in text-base">
+              {blog.toc.map((heading) => {
+                return (
+                  <li key={`#${heading.slug}`} className="py-1">
+                    <a
+                      href={`#${heading.slug}`}
+                      data-level={heading.level}
+                      className="data-[level=two]:pl-0  data-[level=two]:pt-2
+                                       data-[level=two]:border-t border-solid border-dark/40
+                                       data-[level=three]:pl-4
+                                       sm:data-[level=three]:pl-6
+                                       flex items-center justify-start
+                                       "
+                    >
+                      {heading.level === 'three' ? (
+                        <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
+                          &nbsp;
+                        </span>
+                      ) : null}
+
+                      <span className="hover:underline">{heading.text}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </details>
+        </div>
         <RenderMdx blog={blog} />
       </div>
     </article>
