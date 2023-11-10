9270ffbf03dc9543ec52aeb545cbd0ddd60ceb1b
lunaticSKE12 <jamesnapongd@gmail.com>
2023-11-10 15:46:28 +0700

    How to render a Blog

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
-
+import { allBlogs } from 'contentlayer/generated';
 export default function Home() {
+  console.log(allBlogs);
   return (
     <main className="flex flex-col items-center justify-center">
       Hello World!
