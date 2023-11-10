8fd6071d5cb1382c8ca0f04fa3e7d9002ac8530c
lunaticSKE12 <jamesnapongd@gmail.com>
2023-11-10 17:45:25 +0700

    Recent Posts Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { allBlogs } from 'contentlayer/generated';
 import HomeCoverSection from '../components/Home/HomeCoverSection';
 import FeaturedPosts from '../components/Home/FeaturedPosts';
+import RecentPosts from '../components/Home/RecentPosts';
 
 export default function Home() {
   console.log(allBlogs);
@@ -8,6 +9,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
+      <RecentPosts blogs={allBlogs} />
     </main>
   );
 }
